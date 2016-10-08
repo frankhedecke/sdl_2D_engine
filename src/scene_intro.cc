2da413bec70f1fddfc0183b8830026a403bc1654
Frank Hedecke <frank@ifsr.de>
2016-10-08 02:56:34 +0200

    random stars in the background

--- src/scene_intro.cc
+++ src/scene_intro.cc
@@ -1,4 +1,41 @@
+#include <iostream> // TODO remove
+#include <random>
 #include "scene_intro.h"
+
+void* Scene_Intro::ring_alloc(int pos) {
+  // TODO do some deep thinking about ring_buffers, that uses arrays
+  if ((_ring1_cur + 6 - pos) < 0) {
+  }
+}
+
+// tile is 64x64
+SDL_Texture* Scene_Intro::get_tile(int x, int y, uint32_t seed = 0, uint32_t star_count = 5) {
+
+  std::cout << "create tile at " << x << "." << y << std::endl;
+
+  SDL_Surface* surface = SDL_CreateRGBSurface(0, 64, 64, 32, 0, 0, 0, 0);
+  uint32_t* pixels = (uint32_t*) surface->pixels;
+
+
+
+  std::mt19937 generator (seed + x + 97 * y);
+  std::uniform_int_distribution<int> distribution(1,64);
+
+  for (int i = 0; i < star_count; ++i) {
+    int star_x = distribution(generator);
+    int star_y = distribution(generator);
+    std::cout << star_x << "." << star_y << std::endl;
+    *(pixels + 64 * star_x + star_y) = 0xff0000;
+  }
+
+
+
+
+  // SDL_Texture* tex2 = SDL_CreateTexture(_screen->get_renderer(), SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, 64, 64);
+  SDL_Texture* tex = SDL_CreateTextureFromSurface(_screen->get_renderer(), surface);
+
+  return tex;
+}
 
 void Scene_Intro::input(SDL_Event* event) {}
 
@@ -7,6 +44,14 @@ void Scene_Intro::process() {
   // clear screen
   _screen->clear();
 
+  //void render_Texture(float x, float y, float dim_x, float dim_y, SDL_Texture* tex);
+
+  // render background
+  for (int x = 0; x < 10; ++x)
+    for (int y = 0; y < 8; ++y)
+      _screen->render_Texture(0.1 * x, 0.1 * y, 0.1, 0.1, get_tile(x, y));
+
+
   // render text
   _screen->render_Texture(0.2, 0.1, 0.6, 0.1,  _tex_name);
 }
@@ -14,6 +59,7 @@ void Scene_Intro::process() {
 void Scene_Intro::output() {
 
   _screen->present();
+  SDL_Delay(100);
 }
 
 
@@ -55,8 +101,13 @@ Scene_Intro::Scene_Intro(Scene_Manager* manager) : Scene(manager) {
 
   _tex_name = _screen->loadText("a game made with SDL", 48);
   _startup_ticks = SDL_GetTicks();
+  _x_pos = 0;
+  _y_pos = 0;
   for (int i = 0; i < 5; ++i)
     _keys[i] = false;
+  // ring buffer
+  _ring1_cur = 15;
+  ring_alloc(0);
 }
 
 Scene_Intro::~Scene_Intro() {}
--- src/scene_intro.h
+++ src/scene_intro.h
@@ -9,6 +9,10 @@ class Scene_Intro : public Scene {
 
   private:
     uint32_t _startup_ticks;
+    int16_t _x_pos;
+    int16_t _y_pos;
+    void* _ring1[13];
+    int32_t _ring1_cur;
     SDL_Texture* _tex_name;
     // FULLSCREEN
     bool _keys[5];
@@ -16,6 +20,8 @@ class Scene_Intro : public Scene {
     void input(SDL_Event* event);
     void process();
     void output();
+    void* ring_alloc(int pos);
+    SDL_Texture* get_tile(int x, int y, uint32_t seed, uint32_t star_count);
 
   public:
     Scene_Intro(Scene_Manager* manager);
