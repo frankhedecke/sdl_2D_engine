05bac0243938861e4cf1abb4a6078844e15be928
Frank Hedecke <frank@ifsr.de>
2016-10-13 02:32:53 +0200

    cache infrastructre with vectors

--- src/scene_intro.cc
+++ src/scene_intro.cc
@@ -1,17 +1,73 @@
 #include <iostream> // TODO remove
 #include <random>
+#include <vector>
 #include "scene_intro.h"
 
-void* Scene_Intro::ring_alloc(int pos) {
-  // TODO do some deep thinking about ring_buffers, that uses arrays
-  if ((_ring1_cur + 6 - pos) < 0) {
+SDL_Texture* Scene_Intro::init_cache_array(int16_t x, int16_t y) {
+  return nullptr;
+}
+
+SDL_Texture* Scene_Intro::init_cache_vector(int16_t x, int16_t y) {
+
+  _cache.clear();
+
+  int xii = 0;
+
+  for (int16_t xi = x - _x_cache_radius; xi <= _x_cache_window + _x_cache_radius; ++xi) {
+
+    std::vector<SDL_Texture*> column;
+
+    _cache.push_back(column);
+
+    for (int16_t yi = y - _y_cache_radius; yi <= _y_cache_window + _y_cache_radius; ++yi) {
+      std::cout << "cache " << std::endl;
+      _cache.at(xii).push_back(generate_tile(xi, yi, 0 ,5));
+
+      //column.push_back(generate_tile(xi, yi, 0 ,5));
+    }
+    ++xii;
   }
+
+  _x_cache_pos = _x_cache_radius;
+  _y_cache_pos = _y_cache_radius;
+  _x_cache_value = x;
+  _y_cache_value = y;
+
+  std::cout << "cache is new" << std::endl;
+
+
+  for (unsigned i=0; i<_cache.size(); i++)
+    std::cout << _cache.at(i).size() << std::endl;
+
+  return _cache.at(_x_cache_radius).at(_y_cache_radius);
+}
+
+SDL_Texture* Scene_Intro::get_cached(int16_t x, int16_t y) {
+  // TODO do some deep thinking about ring_buffers, that uses arrays
+  // TODO write a cache with 2D array on the heap
+
+  int16_t x_min = _x_cache_value - _x_cache_radius;
+  int16_t x_max = _x_cache_value + _x_cache_radius + _x_cache_window;
+
+  if ((x_min <= x) && (x <= x_max)) {
+    std::cout << "inside x-cache, from " << x_min << " to " << x_max << std::endl;
+    if (_y_cache_value - _y_cache_radius <= y <= _y_cache_value + _y_cache_radius) {
+      // tile inside cache
+
+      return generate_tile(x, y, 0 ,5);
+    }
+  }
+
+  // generate all the cache
+
+  return init_cache_vector(x, y);
 }
 
 // tile is 64x64
-SDL_Texture* Scene_Intro::get_tile(int16_t x, int16_t y, uint32_t seed = 0, uint32_t star_count = 5) {
+SDL_Texture* Scene_Intro::generate_tile(int16_t x, int16_t y, uint32_t seed = 0, uint32_t star_count = 5) {
 
-  //std::cout << "create tile at " << x << "." << y << std::endl;
+  ++_gen_counter;
+  std::cout << "create tile " << _gen_counter << " at " << x << "." << y << std::endl;
 
   SDL_Surface* surface = SDL_CreateRGBSurface(0, 64, 64, 32, 0, 0, 0, 0);
   if (surface == nullptr)
@@ -87,7 +143,8 @@ void Scene_Intro::process() {
   // render background
   for (int x = -2; x < 10; ++x)
     for (int y = -2; y < 8; ++y) {
-      SDL_Texture* tile = get_tile(x + tile_x, y + tile_y);
+      // TODO use cache
+      SDL_Texture* tile = get_cached(x + tile_x, y + tile_y);
       if (tile == nullptr)
         std::cout << "tile is NULL" << std::endl;
       else {
@@ -143,15 +200,20 @@ void Scene_Intro::tick(bool &quit) {
 
 Scene_Intro::Scene_Intro(Scene_Manager* manager) : Scene(manager) {
 
+  _gen_counter = 0;
   _tex_name = _screen->loadText("a game made with SDL", 48);
   _startup_ticks = SDL_GetTicks();
   _x_pos = 0;
   _y_pos = 0;
+  _x_cache_window = 12;
+  _y_cache_window = 10;
+  _x_cache_radius = 2;
+  _y_cache_radius = 2;
   for (int i = 0; i < 5; ++i)
     _keys[i] = false;
   // ring buffer
   _ring1_cur = 15;
-  ring_alloc(0);
+  init_cache_vector(_x_pos / 10, _y_pos / 10);
 }
 
 Scene_Intro::~Scene_Intro() {}
--- src/scene_intro.h
+++ src/scene_intro.h
@@ -1,5 +1,5 @@
 #pragma once
-
+#include <vector>
 #include <SDL.h>
 
 #include "scene.h"
@@ -11,6 +11,16 @@ class Scene_Intro : public Scene {
     uint32_t _startup_ticks;
     int16_t _x_pos;
     int16_t _y_pos;
+    uint32_t _gen_counter;
+    std::vector<std::vector<SDL_Texture*>> _cache;
+    int16_t _x_cache_window;
+    int16_t _y_cache_window;
+    int16_t _x_cache_radius;
+    int16_t _y_cache_radius;
+    int16_t _x_cache_pos;
+    int16_t _y_cache_pos;
+    int16_t _x_cache_value;
+    int16_t _y_cache_value;
     void* _ring1[13];
     int32_t _ring1_cur;
     SDL_Texture* _tex_name;
@@ -20,8 +30,10 @@ class Scene_Intro : public Scene {
     void input(SDL_Event* event);
     void process();
     void output();
-    void* ring_alloc(int pos);
-    SDL_Texture* get_tile(int16_t x, int16_t y, uint32_t seed, uint32_t star_count);
+    SDL_Texture* init_cache_array(int16_t x, int16_t y);
+    SDL_Texture* init_cache_vector(int16_t x, int16_t y);
+    SDL_Texture* get_cached(int16_t x, int16_t y);
+    SDL_Texture* generate_tile(int16_t x, int16_t y, uint32_t seed, uint32_t star_count);
 
   public:
     Scene_Intro(Scene_Manager* manager);
